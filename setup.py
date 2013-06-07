dbe34495e468dce6235575002270ed112fa529ea
Kunal Mehta <legoktm@gmail.com>
2013-06-07 16:17:01 -0700

    Bump up version number to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mtirc',
-    version='0.1.0',
+    version='0.2.0',
     author='Kunal Mehta',
     author_email='legoktm@gmail.com',
     packages=['mtirc'],
